ff581e9a677cb1d53617c196c8b9c194f03ccd52
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-05-01 14:40:33 +0700

    remove unused fetch on dashboard page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -12,13 +12,6 @@ import {
 import CardWrapper from "@/app/ui/dashboard/cards";
 
 export default async function Page() {
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
