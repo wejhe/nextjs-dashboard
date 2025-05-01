18dd617b3ca4f5021e13a20d21057a7d5fd8d9f6
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-05-01 18:40:53 +0700

    implement 1 second waiting time using useDebouncedCallback

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,24 +1,25 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
-    const params = new URLSearchParams(searchParams);
-    
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
 
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
     replace(`${pathname}?${params.toString()}`);
-  };
+  }, 1000);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -31,7 +32,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
